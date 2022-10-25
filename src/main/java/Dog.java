5590d3e763e0afbad04898e647a9bfa32aa6356e
nazar.nerubayko <nazar.nerubayko@ab-soft.net>
2022-10-25 13:25:38 +0300

    -Homework 5

---
+++ src/main/java/Dog.java
@@ -0,0 +1,67 @@
+public class Dog {
+   private int height;
+   private String name;
+   private String color;
+   private int age;
+
+    public Dog(String name, String color, int height,  int age) {
+        this.name = name;
+        this.color = color;
+        if (height >= 10 && height <= 130) {
+            this.height = height;
+        }else {
+            this.height = 80;
+        }
+        if (age >= 0 && age <= 17) {
+            this.age = age;
+        }else {
+            this.age = 7;
+        }
+    }
+
+    public Dog(String name, String color) {
+        this.name = name;
+        this.color = color;
+        this.height = 80;
+        this.age = 7;
+    }
+
+    public Dog() {
+        System.out.println("Creating object of the class Dog.");
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void print(){
+        System.out.printf("Name: %s \nColor: %s \nHeight: %d \nAge: %d \n",name,color,height,age);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Dog dog1 = new Dog("Tuzik","Black", 10,17);
+        dog1.print();
+        System.out.println();
+        Dog dog2 = new Dog("Sharik","White");
+        dog2.print();
+        dog2.setName("Bobi");
+        System.out.println();
+        dog2.print();
+        System.out.println();
+        System.out.println("Name: " + dog1.getName() +" \nColor: "+ dog1.getColor());
+
+    }
+}
