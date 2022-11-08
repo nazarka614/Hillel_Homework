0b6d39be6791c3c990f354793205285b71d0e48c
nazar.nerubayko <nazar.nerubayko@ab-soft.net>
2022-11-08 19:55:46 +0200

    -Homework8

--- src/main/java/homeWork8/Main.java
+++ src/main/java/homeWork8/Main.java
@@ -4,24 +4,14 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        String string = "масло, мас..........ло, масло,te........st,te......st, Nazar,nazar,колбаса....................   .....,molotok, молоко, Кот, Соба.     ........ка, том, Том, рыба, рыба..................";
+        String string = "масло, мас..........ло, масло,te........st,te......st, Nazar,nazar,Roma,roma,roma,колбаса....................   .....,molotok, молоко, Кот, Соба.     ........ка, том, Том, рыба, рыба..................";
         updateString(string);
     }
     private static void updateString(String string){
         String[] words = string.toLowerCase().replace(".", "").replace(" ", "").split(",");
-        HashSet<String> myHashSetList = new HashSet<String>();
+        HashSet<String> myHashSetList = new HashSet<>();
         Collections.addAll(myHashSetList, words);
-        String[] words2 = myHashSetList.toString().split(",");
-        String tempString = "";
-        for (int y = 1; y<=words2.length-1; y++){
-            tempString = words2[y];
-            String result = (tempString.substring(0, 2).toUpperCase() + tempString.substring(2));
-            if (y==words2.length-1) {
-                System.out.println(result.substring(1, result.length() - 1));
-            }else {
-                System.out.println(result.substring(1));
-            }
-        }
+        myHashSetList.forEach(word -> System.out.println(word.substring(0,1).toUpperCase()+word.substring(1)));
     }
 
 }
