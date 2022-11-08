2a5d6807d6faa54b6d4def2ba4912220b925df01
nazar.nerubayko <nazar.nerubayko@ab-soft.net>
2022-11-08 16:41:57 +0200

    -Homework8

--- src/main/java/homeWork8/Main.java
+++ src/main/java/homeWork8/Main.java
@@ -1,28 +1,26 @@
 package homeWork8;
+import java.util.Collections;
 import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        String string = "масло, мас..........ло, масло, колбаса.....   ....., молоко, Кот, Соба.     ........ка, том, Том, рыба, рыба..................";
+        String string = "масло, мас..........ло, масло,te........st,te......st, Nazar,nazar,колбаса....................   ....., молоко, Кот, Соба.     ........ка, том, Том, рыба, рыба..................";
         updateString(string);
-
     }
     private static void updateString(String string){
         string = string.toLowerCase().replace(".", "").replace(" ", "");;
         String[] words = string.split(",");
-        int t =  words.length;
-        HashSet<String> h = new HashSet<String>();
-        for (int i = 0; i<=t-1; i++){h.add(words[i]);
-        }
-        String b = h.toString();
-        String[] words2 = b.split(",");
-        String o = "";
+        HashSet<String> myHashSetList = new HashSet<String>();
+        Collections.addAll(myHashSetList, words);
+        String[] words2 = myHashSetList.toString().split(",");
+        String tempString = "";
         for (int y = 1; y<=words2.length-1; y++){
-            o = words2[y];
-            String result = (o.substring(0, 2).toUpperCase() + o.substring(2));
+            tempString = words2[y];
+            String result = (tempString.substring(0, 2).toUpperCase() + tempString.substring(2));
             if (y==words2.length-1) {
-                System.out.println(y + " " + result.substring(1, result.length() - 1));
+                System.out.println(result.substring(1, result.length() - 1));
             }else {
-                System.out.println(y + " "  + result.substring(1));
+                System.out.println(result.substring(1));
             }
         }
     }
